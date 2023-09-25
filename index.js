76a04104d23369f53b512b832d8f6e9c20cc989d
Samuel Lee <samuel.lee@theoptimum.net>
2023-09-25 17:30:42 +0800

    [MISC][SL] add new constructor to robotsParser

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var { Robots, matches } = require('./Robots');
 
-module.exports.robotsParser = Robots;
+module.exports.robotsParser = function (url, contents) {
+	return new Robots(url, contents);
+};
 module.exports.matches = matches;
